be9d7e85f3da78585d2f18b25e8619bb3ec02cdf
Nick Wiebe <nick@nwiebe.com>
2019-07-21 05:14:38 -0600

    basic skeleton of a compiler

--- prolog.js
+++ prolog.js
@@ -1,3 +1,8 @@
+// utilities
+function unique(array) {
+	return [...new Set(array)];
+}
+
 // node types
 const [VAR, REF, SYM, DOT] = [0, 1, 2, 3];
 
@@ -54,3 +59,45 @@ function *unify(a, b) {
 			yield null;
 	}
 }
+
+// compiler
+function is_var(sym) {
+	return /^[A-Z]/.test(sym);
+}
+function vars(expr) {
+	return unique(expr.flat(Infinity).filter(is_var));
+}
+function compile_var(name) {
+	return `const ${name} = Var();`;
+}
+function compile_expr(expr) {
+	if(expr instanceof Array)
+		if(expr.length === 0)
+			return `Sym("nil")`;
+		else
+			return `Dot(${compile_expr(expr[0])}, ${compile_expr(expr.slice(1))})`;
+	else
+		if(is_var(expr))
+			return expr;
+		else
+			return `Sym("${expr}")`;
+}
+function compile_clause([relation, ...args]) {
+	return `for(const _ of ${relation}(${compile_expr(args)}))`;
+}
+function compile_rule([head, ...clauses]) {
+	return `for(const _ of unify($Args, ${compile_expr(head)}))
+		${clauses.map(compile_clause).join("")}
+		yield null;`;
+}
+function compile_relation(name, rules) {
+	return `function* ${name}($Args) {
+		${vars(rules).map(compile_var).join("")}
+		${rules.map(compile_rule).join("")}
+	}`;
+}
+
+console.log(compile_relation("append", [
+	[[[], "Q", "Q"]],
+	[[["pair", "H", "A"], "B", ["pair", "H", "Q"]], ["append", "A", "B", "Q"]]
+]));
